d5e57fd864c9abb36727f3556d121cd5ca0eebff
Mohamed AAROUS <arosmed3@gmail.com>
2020-04-01 23:51:56 +0200

    The manager: almost finished

--- src/app/components/the-manager/the-manager.component.ts
+++ src/app/components/the-manager/the-manager.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { interval } from 'rxjs';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-the-manager',
@@ -9,7 +10,7 @@ import { interval } from 'rxjs';
 })
 export class TheManagerComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public alertController: AlertController) { }
 
   // step-init
   step: number = 0;
@@ -25,7 +26,9 @@ export class TheManagerComponent implements OnInit {
 
       // wait 1sec then go to step 2
       setTimeout(() => {
-        console.log("dfsdfds")
+        // return to first step and go to
+        // files tab
+        this.step = 0
         this.router.navigate(['/tabs/tab1'])
       }, 1000);
     }
@@ -36,17 +39,49 @@ export class TheManagerComponent implements OnInit {
 
       // wait 1sec then go to step 2
       setTimeout(() => {
-        this.uploadSuccess()
+        this.uploadFileAfter()
       }, 1000);
     }
 
-    // after a successful upload
-    uploadSuccess() {
+    // after a successful or failed upload
+    uploadFileAfter() {
       // step-success
       this.step = 2;
 
       // step-failure
       // this.step = 3;
+    }
+
+    // while enter key is pressed
+    // on code input
+    onCodeEnter() {
+      // step-file-found
+      this.step = 4
+
+      // step-file-not-dound
+      // this.step = 404
+    }
+
+    downloadFile() {
+
+      // afficher jolie alert pour l'user
+      this.downloadingAlert();
+      
+      // go to first step
+      setTimeout(() => {
+        this.step = 0
+      }, 1000);
+    }
+
+    // file is downloading alert
+    async downloadingAlert() {
+      const alert = await this.alertController.create({
+        header: 'Your file is downloading...',
+        message: 'Please check your downloaded files.',
+        buttons: ['OK']
+      });
+  
+      await alert.present();
     }
 
 }
